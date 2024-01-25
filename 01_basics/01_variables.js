84092e9ae0cbc415b2cfbb15a4fff950915508ab
ARYAN SONI <127400418+aryansoni13@users.noreply.github.com>
2024-01-25 19:32:37 +0000

    added variable.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 143434  
+let accounEmail = "dafadf@gmail.com" 
+var accountPassword = "dfad#@#@3156"
+accountCity = "Delhi"
+
+// accountId = 2 // not allowed 
+
+accounEmail = "adasdfsd@gmail.com"
+accountPassword = "adfe2e2"
+accountCity = "Mohali"
+
+console.log(accountId);
+
+console.table([accounEmail, accountId, accountPassword, accountCity])
