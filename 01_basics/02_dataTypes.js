107172d47f0e66e74623581e3aa2675bc74175bb
ARYAN SONI <127400418+aryansoni13@users.noreply.github.com>
2024-01-25 19:53:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs not browser 
+
+console.log(3 
+    + 3) // code readability should be high 
+console.log("Aryan")
+
+let name = "Aryan"
+let age = 19
+let isLoggedIn = false
+let state; 
+
+// number ==> 2 to power 53 
+// bigint 
+// string ==> " " or ' '
+// boolean ==> either true or false 
+// null ==> stans alone value 
+// undiefind  ==>
+// symbol ==> unique 
+
+// object 
+
+console.log(typeof "Aryan");
+console.log(typeof(age));
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
+
+
