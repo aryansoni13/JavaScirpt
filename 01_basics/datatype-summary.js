b9fea79156d12adf18993fede1507686e472ac16
ARYAN SONI <arusoni477@gmail.com>
2024-07-09 13:33:37 +0530

    Updated datatype-summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -4,34 +4,61 @@
 
 // JavaScript is a dynamically typed language 
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+// // console.log(id === anotherId);
 
-const bigNumber = 346545645556749875646546n
+// const bigNumber = 346545645556749875646546n
 
 
-//  Reference (Non Primitve)
+// //  Reference (Non Primitve)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heros = ("shaktiman", "naagraj", "doga");
+// const heros = ("shaktiman", "naagraj", "doga");
 
-let myObj = {
-    name: "Aryan",
-    age: 20,
+// let myObj = {
+//     name: "Aryan",
+//     age: 20,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello");
+// }
+
+// console.log(typeof outsideTemp);
+
+
+
+// ************** Memory ******************
+
+// Stack (Primitive), Heap(Non Primitive)
+
+let myYoutubename = "xyz"
+
+let anothername  = myYoutubename
+anothername  = "hadafd"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello");
-}
+let userTwo = userOne
 
-console.log(typeof outsideTemp);
+
+userTwo.email = "xyz@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
